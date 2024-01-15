d5879eabd84a792e778312d617bfdb9575820f86
Jaraf Salma <115297113+salmajaraf@users.noreply.github.com>
2024-01-15 08:40:24 +0100

    Modif in home

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,4 +1,18 @@
 @extends('layouts.master2')
 @section('body')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home</title>
+    <link rel="stylesheet" href="{{ asset('css/banner.css') }}">
 
+</head>
+<body>
+    <div  class="banner" id="home">
+        <h1 class="hometitre">InnoTech</h1>
+        <a href="#">Créer Un Compte</a>
+    </div>
+</body>
+</html>
 @endsection
