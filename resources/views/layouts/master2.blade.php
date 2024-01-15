e14f0892b604f7def4693c2021c364b931c43e29
YE <youssef.ejjiyar-etu@etu.univh2c.ma>
2024-01-15 08:43:08 +0100

    changing the header meta

--- resources/views/layouts/master2.blade.php
+++ resources/views/layouts/master2.blade.php
@@ -2,17 +2,8 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <!-- resources/views/navbar.blade.php -->
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Header</title>
+    <title>InnoTech</title>
+    
     <link rel="stylesheet" href="{{ asset('css/nav.css') }}">
     <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
     <link rel="stylesheet" href="{{ asset('css/collab.css') }}">
